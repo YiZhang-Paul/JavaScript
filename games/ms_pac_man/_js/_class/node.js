efa4ed7c8768ebc5115b2225b337cc199de13679
YI ZHANG <scaccterran@gmail.com>
2017-06-06 11:51:10 -0400

    node class for path finding

---
+++ games/ms_pac_man/_js/_class/node.js
@@ -0,0 +1,14 @@
+/* jslint esversion: 6 */
+/**
+ * node class
+ * @param int, int
+ *
+ * row    : row of node
+ * column : column of node
+ */
+class Node {
+	constructor(row, column) {
+		this.row = row;
+		this.column = column;
+	}
+} 
